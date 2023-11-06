2d34e581cc30fd145f7c5e822a0c9e0a59029674
JuFMacedo <julia_freitasmacedo@hotmail.com>
2023-11-06 01:43:05 -0300

    Adição de Carrossel na Home

--- e-commerce-g6/src/Pages/Home/index.jsx
+++ e-commerce-g6/src/Pages/Home/index.jsx
@@ -3,21 +3,53 @@ import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import { Container } from "react-bootstrap";
 import DarkMode from "../../Components/DarkMode";
+import firstImage from "../../assets/treino.jpg";
+import secondImage from "../../assets/treino.jpg";
+import thirdImage from "../../assets/Serrafy.png";
+import { Carousel } from "react-bootstrap";
+import "./home.css";
 
 const Home = () => {
   return (
-    <Container fluid className="p-0">
-      <DarkMode />
+    <div className="home-container">
       <header>
         <Header />
       </header>
-      <section className="pgHome">
-        <h1>Teste</h1>
-      </section>
+
+      <Container fluid className="carousel-container">
+        <Carousel fade className="carousel-rectangular-landscape">
+          <Carousel.Item>
+            <img
+              src={firstImage}
+              alt="First slide"
+              className="carousel-image"
+            />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src={secondImage}
+              alt="Second slide"
+              className="carousel-image"
+            />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src={thirdImage}
+              alt="Third slide"
+              className="carousel-image"
+            />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+
       <footer>
         <Footer />
       </footer>
-    </Container>
+      <DarkMode />
+    </div>
   );
 };
 
