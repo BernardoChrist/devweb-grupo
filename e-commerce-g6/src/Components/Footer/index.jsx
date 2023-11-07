397e762c006dc34327ab6626cee4afc02a918924
JuFMacedo <julia_freitasmacedo@hotmail.com>
2023-11-06 21:23:30 -0300

    Atualização Footer

--- e-commerce-g6/src/Components/Footer/index.jsx
+++ e-commerce-g6/src/Components/Footer/index.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./style.css";
+import {
+  AiFillInstagram,
+  AiFillFacebook,
+  AiFillYoutube,
+  AiOutlineTwitter,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { BsSpotify, BsTiktok } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer>
-      <address className="insta">Intagram: @serrafy </address>
-      <address className="face">Facebook: Serrafy</address>
+      <address className="logos">
+        <AiFillInstagram />
+        <AiFillFacebook />
+        <AiFillYoutube />
+        <BsSpotify />
+        <BsTiktok />
+        <AiOutlineTwitter />
+        <AiFillLinkedin />
+      </address>
     </footer>
   );
 };
