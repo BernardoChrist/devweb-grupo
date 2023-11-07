e2ec894b535c033b94dcb42f4595a9f52abc3d34
LilianeDavid93 <lilianemarquesdavid@gmail.com>
2023-11-06 22:34:23 -0300

    DarkMode resolvendo conflito

--- e-commerce-g6/src/App.jsx
+++ e-commerce-g6/src/App.jsx
@@ -10,7 +10,6 @@ import { ThemeContextProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-<<<<<<< HEAD
     <ThemeContextProvider>
       <Routes>
         <Route path="/serrafy" element={<Serrafy />} />
@@ -21,17 +20,6 @@ function App() {
         <Route path="/editar" element={<EditarMusica />} />
       </Routes>
     </ThemeContextProvider>
-=======
-    <Routes>
-      <Route path="/serrafy" element={<Serrafy />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/home/:user" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/contato" element={<Contato />} />
-      <Route path="/editar" element={<EditarMusica />} />
-      <Route path="*" element={<h1>Pagina não encontrada</h1>} />
-    </Routes>
->>>>>>> e103f529e5065d655e9fbc602d45eae777554c05
   );
 }
 
--- e-commerce-g6/src/Components/Footer/index.jsx
+++ e-commerce-g6/src/Components/Footer/index.jsx
@@ -1,16 +1,31 @@
 import { React, useContext } from "react";
 import "./style.css";
 import { ThemeContext } from "../../context/ThemeContext";
+import {
+  AiFillInstagram,
+  AiFillFacebook,
+  AiFillYoutube,
+  AiOutlineTwitter,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { BsSpotify, BsTiktok } from "react-icons/bs";
 
 const Footer = () => {
   const { isDarkMode } = useContext(ThemeContext);
   return (
     <footer className={isDarkMode ? "bg-dark" : "bg-light"}>
-      <address className={`insta ${isDarkMode ? "text-light" : "text-dark"}`}>
-        Intagram: @serrafy{" "}
-      </address>
-      <address className={`face ${isDarkMode ? "text-light" : "text-dark"}`}>
-        Facebook: Serrafy
+      <h3 className={isDarkMode ? "text-light" : "text-dark"}>© Serrafy</h3>
+
+      <address
+        className={`logos gap-2 ${isDarkMode ? "text-light" : "text-dark"}`}
+      >
+        <AiFillInstagram />
+        <AiFillFacebook />
+        <AiFillYoutube />
+        <BsSpotify />
+        <BsTiktok />
+        <AiOutlineTwitter />
+        <AiFillLinkedin />
       </address>
     </footer>
   );
